0969ed600fe3030992f04bcb5c136a15dff8f108
zjnxyz <469174375@qq.com>
2013-02-28 11:35:04 +0800

    all code bootstrop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopBoot::Application.config.secret_token = 'aa830d632451b2ca43bdfd40903ececd4e46044ec3f2d54b18459be039a5a271d53366e2847fe3bd781300c2297cb5a159e3616077f4719e349c416a9c043890'
